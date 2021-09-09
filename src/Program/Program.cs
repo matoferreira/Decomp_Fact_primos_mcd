c5035656f29d31a151c81e2a57b5e7114e70a74c
matoferreira <matifer1@gmail.com>
2021-09-09 15:46:47 -0300

    avanzado MCD en funcionamiento

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,33 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Pido al usuario que ingrese los números
+            Console.WriteLine("Ingrese el primer número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Comparo los dos números y verifico cual es el mayor
+            //al mayor le asigno la variable "a", al menor la variable "b"
+
+            int a = Math.Max (num1, num2);
+            int b = Math.Min (num1, num2);
+
+            //Creo una variable resultado
+            int resultado;
+
+
+            //Aplicando teorema de euclides
+            do
+            {
+                resultado = b;
+                b = a%b;
+                a = resultado;  
+            } while (b!=0);
+
+            //Mostramos como resultado el último resto no nulo
+            Console.WriteLine($"El mcd entre {num1} y {num2} es: {resultado}");
+            Console.ReadKey(true);
         }
     }
 }
